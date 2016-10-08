b29d32acdd1ce94b1a112e69fb413c4d74e910a0
Mahesh <mahesh.chitroda@hotmail.com>
2016-10-08 22:11:41 +0530

    First commit of JavaScript

---
+++ JavaScript/JavaScript/Controllers/JavaScriptController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JavaScript.Controllers
+{
+    public class JavaScriptController : Controller
+    {
+        // GET: JavaScript
+        public ActionResult Function()
+        {
+            return View();
+        }
+
+        public ActionResult VarAndLet()
+        {
+            return View();
+        }
+    }
+}
---
+++ JavaScript/JavaScript/Scripts/JavaScript/Function.js
@@ -0,0 +1,20 @@
+﻿//anonymous function 
+(function () {
+
+    alert("I am anonymous function");
+
+    // *********************  function declaration ***********************************
+    alert("Function Declaration :-" + functionDeclaration(5)); // function delcarion can call like this.
+    function functionDeclaration(value) {
+        return value + value;
+    }
+
+
+    // ***************  function expression ************************************************
+    // alert("FunctionExpression :-" + functionExpression(5)); Will not work.You can not call function expression in this place.
+    var functionExpression = function (a) { return a * a; };
+    alert("FunctionExpression :-" + functionExpression(5));
+
+})();
+
+// () is a IIFE = imediate invoke function expression which invoke function when Javascript is register in web page
