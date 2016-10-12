25e571cf53c3c1c4c1ff7a7a20de68ddeb5b9bf5
Mahesh <mahesh.chitroda@hotmail.com>
2016-10-13 00:08:47 +0530

    Adding Variable scope example

---
+++ TypeScript/TypeScript/VariablesScope.ts
@@ -0,0 +1,37 @@
+﻿namespace mySpace {
+    const x: number = 10;
+}
+const y: number = 20;
+
+class VariableDeclaration {
+    //const static x: number = 10; you can not declare constant variable inside class
+}
+
+function Var(y: number) {
+    var x;
+    //let x; can't declare same variable with var and let
+    var x;
+    for (let i = 0; i < 5; i++) {
+        let y: string; // shadowing parameter y
+    }
+    //alert(i); i is not accessible here. i variable is delcared using let and scope of i is for loop
+    for (var i = 0; i < 5; i++) {
+
+    }
+    alert(i);// var i scope is whole function.
+    //a = 10; // can't use a variable.First we need to declare it and then we can use it.
+    //let a;
+
+    a = 10; // in case of var, we can first assign value to variable and then we can declare it.
+    var a;
+
+    let functionType = function () { //  nested function. here the type of varialbe x is function.
+        alert("function inside function");                   //  and we can call this nested function with functionType().
+    }
+    functionType();
+}
+
+window.onload = () => {
+    let varialbe = new VariableDeclaration();
+    Var(10);
+};
